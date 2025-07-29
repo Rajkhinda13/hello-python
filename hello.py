ea95b5febf351779f1096c70a22f803d58f9f670
RAJ <raj@RAJs-MacBook-Air.local>
2025-07-29 12:44:44 +0530

    Add even or odd number checker

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
 print("Hello, GitHub!")
+# even_odd.py
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is Even.")
+else:
+    print("The number is Odd.")
+# calculator.py
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+op = input("Choose operation (+, -, *, /): ")
+
+if op == '+':
+    print("Result:", a + b)
+elif op == '-':
+    print("Result:", a - b)
+elif op == '*':
+    print("Result:", a * b)
+elif op == '/':
+    if b != 0:
+        print("Result:", a / b)
+    else:
+        print("Cannot divide by zero!")
+else:
+    print("Invalid operator!")
